bb976dae8448a5f61620720f751d9e663daf30eb
U-Alex-PC\Alex <Alex@Alex-PC.(none)>
2013-07-05 22:40:14 -0500

    Broken build, retract won't take parameters.

--- Source/Claw.hx
+++ Source/Claw.hx
@@ -13,18 +13,32 @@ import nme.Lib;
  */
 class Claw extends Sprite
 {
-
-	public function new() 
-	{
+	private var baseX:Float = 0;
+	private var baseY:Float = 0;
+	
+	public function new() {
 		super();
-		addChild(new Bitmap(Assets.getBitmapData("assets/meat.png")));
+		addChild(new Bitmap(Assets.getBitmapData("assets/small/claw.png")));
 		Lib.stage.addEventListener(MouseEvent.MOUSE_DOWN, onClick);
 	}
 	
-	public function onClick(event:MouseEvent)
-	{
-		
-		Actuate.tween(this, 1, { x: event.stageX } ).ease(Linear.easeNone);
-		Actuate.tween(this, 1, { y: event.stageY } ).ease(Linear.easeNone);
+	public function onClick(event:MouseEvent) {
+		var distance:Float = distance(baseX, baseY, event.stageX, event.stageY);
+		trace(distance);
+		var time:Float = distance / 1000.0;
+		trace(time);
+		Actuate.tween(this, time, { x: event.stageX } ).ease(Linear.easeNone);
+		Actuate.tween(this, time, { y: event.stageY } ).ease(Linear.easeNone).onComplete(retract, time);
+	}
+	
+	public function retract(time:Float ) {
+		Actuate.tween(this, time, { x: baseX } ).ease(Linear.easeNone);
+		Actuate.tween(this, time, { y: baseY } ).ease(Linear.easeNone);
+	}
+	
+	private function distance(x1:Float, y1:Float, x2:Float, y2:Float):Float {
+		var x_ = x2 - x1;
+		var y_ = y2 - y1;
+		return Math.sqrt(x_ * x_ + y_ * y_);
 	}
 }
